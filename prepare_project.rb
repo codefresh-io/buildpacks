08647c41726a7cde17e5cb109be15f24eca9cdd8
Oleg <oleg@codefresh.io>
2015-08-25 14:16:04 +0300

    Update prepare_project.rb

--- prepare_project.rb
+++ prepare_project.rb
@@ -1,33 +1,20 @@
-def params_yml(params = {})
-  param_yml = ''
-  params.each do |key, value|
-    param_yml += "#{key} : #{value.first} \n"
-  end
-  param_yml
-end
-
-def attribute_yml name, value, force_string = false
+def attribute_yml value, force_string = false
   if value
-    value_string =
-        if force_string
-          '"' + value + '"'
-        else
-          value
-        end
-    "#{name}: #{value_string}"
+    force_string ? '"' + value + '"' : value
   else
-    ""
+    ''
   end
 end
 
 def create_database_yml
   require 'cgi'
   require 'uri'
+  require 'yaml'
 
   begin
     uri = URI.parse(ENV["DATABASE_URL"])
   rescue URI::InvalidURIError
-    uri = URI.parse("postgres://postgresql@localhost/code_fresh_prod")
+    uri = URI.parse("postgres://root:root@localhost/code_fresh_prod")
   end
 
   adapter  = uri.scheme
@@ -38,42 +25,27 @@ def create_database_yml
   host     = uri.host
   port     = uri.port
   params   = CGI.parse(uri.query || "")
-  File.open("config/database.yml", "w") do |file|
-    file.puts <<-DATABASE_YML
-production:
-#{attribute_yml "adapter",  adapter}
-    #{attribute_yml "database", database}
-    #{attribute_yml "username", username}
-    #{attribute_yml "password", password, true if password}
-    #{attribute_yml "host",     host}
-    #{attribute_yml "port",     port}
-development:
-  adapter: postgresql
-  encoding: utf8
-  database: code_fresh_develop
-  pool: 5
-  username: root
-  password: root
-  min_messages: warning
-test:
-  adapter: postgresql
-  encoding: utf8
-  database: code_fresh_test
-  pool: 5
-  username: root
-  password: root
-  min_messages: warning
-#{params_yml(params)}
-    DATABASE_YML
+
+
+  databases_conn = YAML.load_file "config/database.yml"
+  databases_conn.each do |databse_conn|
+    databse_conn[1]["username"] = attribute_yml 'root'
+    databse_conn[1]["password"] = attribute_yml 'root'
   end
+
+  databases_conn["production"]["adapter"] = attribute_yml  adapter
+  databases_conn["production"]["database"] = attribute_yml  database
+  databases_conn["production"]["username"] = attribute_yml  username
+  databases_conn["production"]["password"] = attribute_yml  password, true if password
+  databases_conn["production"]["host"] = attribute_yml  host
+  databases_conn["production"]["port"] = attribute_yml  port
+  File.open("config/database.yml", 'w') { |f| YAML.dump(databases_conn, f) }
 end
 
 def add_12_factor
-  unless File.foreach("Gemfile").grep(/rails_12factor/).any?
-    File.open("Gemfile", "a") do |file|
-      file.puts "gem 'rails_12factor', group: :production"
-      file.puts "gem 'foreman'"
-    end
+  File.open("Gemfile", "a") do |file|
+    file.puts "gem 'rails_12factor', group: :production"
+    file.puts "gem 'foreman'"
   end
 end
 
